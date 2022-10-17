0a5b65cf6a799f2703f88de2dfc34ccfd5b031ed
mustofa354 <mustofafirdaus354@gamil.com>
2022-10-17 22:41:51 +0700

    Menyelesaikan Tugas 4 Video 3 - Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return  view('home');
+});
+
+Route::get('/about',function(){
+    return  view('about', [
+        "name" => "Mustofa Firdaus",
+        "email" => "mustofafirdaus01@gmail.com",
+        "image" => "mustofa.jfif"
+    ]);
+});
+
+Route::get('/blog',function(){
+    return  view('posts');
 });
