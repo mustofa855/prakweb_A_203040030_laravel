bfd0f57a4d0d8fb25fff7f1619726a4dbd1bc1cc
mustofa354 <mustofafirdaus354@gamil.com>
2022-10-26 21:31:41 +0700

    Menyelesaikan Tugas 5 Video 9 - Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,65 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Mustofa Firdaus',
+            'email' => 'mustofa@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Muhammad David',
+            'email' => 'david123@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae, eius facere ad? Facilis molestias ex, non eos molestiae nesciunt ab mollitia doloribus, sapiente quisquam culpa? Modi numquam, dolor laborum officia velit neque praesentium doloribus quibusdam nisi rem tempora beatae expedita quam. Asperiores, veniam officiis! Magni quia excepturi cum commodi, hic culpa reprehenderit pariatur assumenda tempore saepe minima nisi enim suscipit incidunt temporibus maxime! Dolor placeat nihil quidem reiciendis ab odio totam iure repudiandae aperiam repellat, soluta at sed nesciunt rem quam iusto!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-Kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae, eius facere ad? Facilis molestias ex, non eos molestiae nesciunt ab mollitia doloribus, sapiente quisquam culpa? Modi numquam, dolor laborum officia velit neque praesentium doloribus quibusdam nisi rem tempora beatae expedita quam. Asperiores, veniam officiis! Magni quia excepturi cum commodi, hic culpa reprehenderit pariatur assumenda tempore saepe minima nisi enim suscipit incidunt temporibus maxime! Dolor placeat nihil quidem reiciendis ab odio totam iure repudiandae aperiam repellat, soluta at sed nesciunt rem quam iusto!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-Ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae, eius facere ad? Facilis molestias ex, non eos molestiae nesciunt ab mollitia doloribus, sapiente quisquam culpa? Modi numquam, dolor laborum officia velit neque praesentium doloribus quibusdam nisi rem tempora beatae expedita quam. Asperiores, veniam officiis! Magni quia excepturi cum commodi, hic culpa reprehenderit pariatur assumenda tempore saepe minima nisi enim suscipit incidunt temporibus maxime! Dolor placeat nihil quidem reiciendis ab odio totam iure repudiandae aperiam repellat, soluta at sed nesciunt rem quam iusto!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-Keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae,',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed commodi voluptas dicta ea exercitationem non distinctio voluptatem obcaecati, veniam temporibus dolorum error in quae quo ut vitae, eius facere ad? Facilis molestias ex, non eos molestiae nesciunt ab mollitia doloribus, sapiente quisquam culpa? Modi numquam, dolor laborum officia velit neque praesentium doloribus quibusdam nisi rem tempora beatae expedita quam. Asperiores, veniam officiis! Magni quia excepturi cum commodi, hic culpa reprehenderit pariatur assumenda tempore saepe minima nisi enim suscipit incidunt temporibus maxime! Dolor placeat nihil quidem reiciendis ab odio totam iure repudiandae aperiam repellat, soluta at sed nesciunt rem quam iusto!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
+        
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,9 @@
 <article>
    <h1 class="mb-5">{{ $post->title }}</h1>
 
-   <p>By. Mustofa Firdaus in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+   <p>By. <a class="text-decoration-none" href="#">{{ $post->user->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
-<a href="/blog">Back to Posts</a>
+<a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
 
