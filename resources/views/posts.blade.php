c25c076c4fdf837419830555ef92b089105b9369
mustofa354 <mustofafirdaus354@gamil.com>
2022-11-01 13:51:33 +0700

    Memperbaiki bug pada Tugas 6 Video 13 - Searching & Pagination

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 
 <div class="row justify-content-center mb-3">
   <div class="col-md-6">
-    <form action="/blog">
+    <form action="/posts">
       @if (request('category'))
         <input type="hidden" name="category" value="{{ request('category') }}">
       @endif
@@ -15,7 +15,7 @@
       @endif
       <div class="input-group mb-3">
         <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
-        <button class="btn btn-primary" type="submit">Search</button>
+        <button class="btn btn-danger" type="submit">Search</button>
       </div>
     </form>
   </div>
@@ -28,7 +28,7 @@
       <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
       <p>
         <small class="text-muted">
-          By. <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+          By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
         </small>
       </p>
       <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -42,13 +42,13 @@
       @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
           <div class="card">
-            <div class="position-absolute px-3 py-2 " style="background-color : rgba(0, 0, 0, 0.7)"><a href="/blog?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+            <div class="position-absolute px-3 py-2 " style="background-color : rgba(0, 0, 0, 0.7)"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
             <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
             <div class="card-body">
               <h5 class="card-title">{{ $post->title }}</h5>
               <p>
                 <small class="text-muted">
-                  By. <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                  By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                 </small>
               </p>
               <p class="card-text">{{ $post->excerpt }}</p>
